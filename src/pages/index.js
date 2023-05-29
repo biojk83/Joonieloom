ed0c5ed01fee20579e4469378897e3061b0e4e57
Youngwok Kim <youngwok.kim83@gmail.com>
2023-05-29 18:54:09 -0400

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,8 +46,11 @@ const IndexPage = () => {
           wear by <span className={styles.gold}>sunspel</span> and{' '}
           <span className={styles.gold}>scotch&soda</span>
         </p>
-         {/* Ecwid Store */}
-        <div id="my-store-86415007"></div>
+      </div>
+
+      {/* Ecwid */}
+      <div>
+      <div id="my-store-86415007"></div>
         <div>
           <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?86415007&data_platform=code&data_date=2023-05-29" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-86415007");</script>
         </div>
