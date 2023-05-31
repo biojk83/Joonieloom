b32fdaeb33587b0e79a6c3fe8c6efcc6fe88833d
biojk83 <youngwok.kim83@gmail.com>
2023-05-30 20:20:25 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import ProductCollectionGrid from '../components/ProductCollectionGrid';
 import ProductCardGrid from '../components/ProductCardGrid';
 import Quote from '../components/Quote';
 import Title from '../components/Title';
-import {ProductBrowser} from '@ecwid/gatsby-plugin-ecwid'
+
 
 import { generateMockBlogData, generateMockProductData } from '../helpers/mock';
 
@@ -47,11 +47,7 @@ const IndexPage = () => {
           wear by <span className={styles.gold}>sunspel</span> and{' '}
           <span className={styles.gold}>scotch&soda</span>
         </p>
-        <>
-          <ProductBrowser
-          storeId="86415007"
-      />
-        </>
+       
       </div>
 
       {/* Collection Container */}
